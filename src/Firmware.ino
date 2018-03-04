7f1cb1526ac2228648d3794f5d79d9f458bda84d
yovany <yovany.ocampo1@gmail.com>
2018-03-04 18:11:12 -0500

    Detection ready

--- src/Firmware.ino
+++ src/Firmware.ino
@@ -3,12 +3,19 @@
 #define RS485Receive LOW
 #define RS485Serial Serial2
 
+#define interruptPin 20
+
+volatile unsigned long tiempo = 0;
+volatile unsigned long retardo = 0;
+
 int byteIO;
 
 void setup() {
+  attachInterrupt(digitalPinToInterrupt(interruptPin), interruption, FALLING);
   // initialize both serial ports:
   Serial.begin(9600);
   Serial.write("Communication ready");
+  Serial.write("Detection ready");
   RS485Serial.begin(9600);
   pinMode(RS485ControlIO, OUTPUT);
   digitalWrite(RS485ControlIO, RS485Receive);
@@ -27,4 +34,16 @@ void loop() {
     Serial.write(byteIO);
     delay(10);
   }
+}
+
+void interruption() {
+  retardo = millis() - tiempo;
+  if (retardo > 120 || retardo < 40) {
+    Serial.write("Detected");
+    digitalWrite(RS485ControlIO, RS485Transmit);
+    RS485Serial.write("D");
+    delay(10);
+    digitalWrite(RS485ControlIO, RS485Receive);
+  }
+  tiempo = millis();
 }
